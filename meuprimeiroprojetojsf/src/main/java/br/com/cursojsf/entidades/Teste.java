aab56db9cd810b58cd910192de4700c46e3c4574
Leandro <Leandro@DESKTOP-SHKFPNB>
2021-01-06 14:16:35 -0300

    Foi criado a página xhtml de teste, a entidade teste e adicionado no template menu.

---
+++ meuprimeiroprojetojsf/src/main/java/br/com/cursojsf/entidades/Teste.java
@@ -0,0 +1,9 @@
+package br.com.cursojsf.entidades;
+
+import java.io.Serializable;
+
+public class Teste extends GenericDomain implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+}
